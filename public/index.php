0b8387245fd3932875e81ced7b6b88607cc022b7
Kelyddon <kelyddon.diemunsch@gmail.com>
2025-02-19 20:42:08 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,5 @@ require_once '../functions/auth.php';
 require_once '../functions/quiz.php';
 
 include '../includes/header.phtml';
-include '../includes/navbar.phtml';
 include '../includes/index.phtml';
 include '../includes/footer.phtml';
-?>
