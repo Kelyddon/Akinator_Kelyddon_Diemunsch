6fc1200449097944dd99d19adc307938b8dd3b46
Kelyddon <kelyddon.diemunsch@gmail.com>
2025-02-19 21:51:58 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,12 +1,8 @@
 <?php
-$host = 'db.3wa.io';
-$dbname = 'kelyddondiemunsch_test_akinator_fonctionnel';
-$username = 'kelyddondiemunsch';
-$password = '5721e53e81737dcfcce9389af64949e4';
-
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    $pdo= new PDO('mysql:host=db.3wa.io;port=3306;dbname=kelyddondiemunsch_Akinator;charset=utf8', 'kelyddondiemunsch', '5721e53e81737dcfcce9389af64949e4');
+    $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOExeption $e){
+    die("Connection failed;" . $e->getMessage());
 }
