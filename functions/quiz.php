3479852a74371eab58f86b62d24ebc6fa47bde91
Kelyddon <kelyddon.diemunsch@gmail.com>
2025-02-19 22:27:14 +0100

    Update quiz.php

--- functions/quiz.php
+++ functions/quiz.php
@@ -3,28 +3,26 @@ require_once '../config/database.php';
 
 function getFirstQuestion() {
     global $pdo;
-    $log = $pdo -> query("SELECT * FROM questions WHERE first_question = TRUE LIMIT 1");
-    
+    $log = $pdo->query("SELECT * FROM questions WHERE first_question = TRUE LIMIT 1");
     if ($log) {
-        return $log -> fetch(PDO::FETCH_ASSOC);
-    } 
-    else {
+        return $log->fetch(PDO::FETCH_ASSOC);
+    } else {
         return false;
     }
 }
 
 function getNextQuestion($question_id, $answer) {
     global $pdo;
-    $log = $pdo -> prepare("SELECT * FROM answer WHERE question_id = :question_id AND text = :answer LIMIT 1");
+    $log = $pdo->prepare("SELECT * FROM answer WHERE question_id = :question_id AND text = :answer LIMIT 1");
     
-    if ($log -> execute(['question_id' => $question_id, 'answer' => $answer])) {
-        $answer = $log -> fetch(PDO::FETCH_ASSOC);
+    if ($log->execute(['question_id' => $question_id, 'answer' => $answer])) {
+        $answer = $log->fetch(PDO::FETCH_ASSOC);
         
         if ($answer && $answer['next_question_id']) {
-            $log = $pdo -> prepare("SELECT * FROM questions WHERE id = :id LIMIT 1");
+            $log = $pdo->prepare("SELECT * FROM questions WHERE id = :id LIMIT 1");
             
-            if ($log -> execute(['id' => $answer['next_question_id']])) {
-                return $log -> fetch(PDO::FETCH_ASSOC);
+            if ($log->execute(['id' => $answer['next_question_id']])) {
+                return $log->fetch(PDO::FETCH_ASSOC);
             } 
             else {
                 return false;
